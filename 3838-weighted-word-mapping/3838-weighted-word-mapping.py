ff3225912bece773fec485f91a9fa85dd3503cc9
supremed-data <supremed.data@gmail.com>
2026-02-22 21:43:38 +0900

    Time: 19 ms (48.82%), Space: 12.4 MB (93.36%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.py
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.py
@@ -5,22 +5,17 @@ class Solution(object):
         :type weights: List[int]
         :rtype: str
         # """
-        # TODO: 이 부분 효율화
-        mapped = "abcdefghijklmnopqrstuvwxyz"
-        reversed_mapped = mapped[::-1]
-        result = ""
+        result = []
         
         for word in words:
             weight = 0
+            base = ord("a")
+            z = ord("z")
 
-            for letter in word:
-                # TODO: index 계산
-                letter_ind = ord(letter) - 97
-                weight += weights[letter_ind]
+            for char in word:
+                weight += weights[ord(char) - base]
 
-            # TODO: mapped character 구해서 더하기
             modulo = weight % 26
-            mapped_char = reversed_mapped[modulo]
-            result += mapped_char
-
-        return result
+            result.append(chr(z - modulo))
+            
+        return "".join(result)
